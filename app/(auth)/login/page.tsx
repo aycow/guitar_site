2c3ae17d790ae1bbdb6fa5db2364308bf798162e
Ali Chen <alichen247@gmail.com>
2026-02-21 16:24:30 -0500

    edited login page and added tailwind

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,17 @@
 "use client";
 
 export default function LoginPage() {
-  return <main>Login</main>;
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-b from-white to-red-100 whitespace-nowrap">
+      
+      <div className="flex text-center max-w-xl px-4 items-center justify-center">
+        
+        <h1 className="text-6xl font-bold text-black select-none">
+          Welcome to Guitar Site
+        </h1>
+
+      </div>
+
+    </main>
+  );
 }
